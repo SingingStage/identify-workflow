e65619dd56c1a8a1cf0e7716d203545e06bbd7f4
ophirus <dreckguy@gmail.com>
2025-01-29 19:07:49 -0500

    fix input logs

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 async function run() {
   try {
-    // Access the context to get inputs
-    const inputs = github.context.payload.inputs;
+    // Get all input names dynamically
+    const inputs = {};
+    const inputNames = Object.keys(process.env).filter(key => key.startsWith('INPUT_'));
 
-    if (!inputs) {
+    inputNames.forEach((envVar) => {
+      const inputName = envVar.replace('INPUT_', '').toLowerCase(); // Convert to lowercase
+      inputs[inputName] = process.env[envVar];
+    });
+
+    if (Object.keys(inputs).length === 0) {
       console.log('No inputs were provided to this workflow.');
       return;
     }
 
     // Print each input
-    console.log('Workflow dispatch inputs:');
+    console.log('Workflow inputs:');
     for (const [key, value] of Object.entries(inputs)) {
       console.log(`${key}: ${value}`);
     }
